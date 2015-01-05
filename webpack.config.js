fece187e7f09fb406ce8e9f3092244167505d1f4
rasmusfl0e <rasmusfl0e@gmail.com>
2015-01-05 01:52:37 +0100

    demo content

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	target: "web",
+	debug: false,
+	entry: {
+		index: "./index.js",
+	},
+	output: {
+		path: "./dist/",
+		filename: "[name].bundle.js",
+		publicPath: "/dist/"
+	},
+	module: {
+		loaders: [
+			{ test: /\.css/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") }
+		]
+	},
+	plugins: [
+		new UglifyJsPlugin(),
+		new ExtractTextPlugin("[name].css")
+	]
+};
