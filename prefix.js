a1e040825009ba010d72088116e5d8f8fe9821f8
rasmusfl0e <rasmusfl0e@gmail.com>
2015-01-05 01:24:17 +0100

    reverse prefix order

--- prefix.js
+++ prefix.js
@@ -2,9 +2,10 @@ var style = document.createElement("p").style;
 var prefixes = " -o -ms -moz -webkit".split(" ");
 
 module.exports = function (property) {
-	var i = prefixes.length
-	while (i--) {
-		var name = prefixes[i] + property;
+	var i = 0;
+	var l = prefixes.length
+	while (i < l) {
+		var name = prefixes[i++] + property;
 		if (style[name] !== undefined) {
 			return name;
 		}
